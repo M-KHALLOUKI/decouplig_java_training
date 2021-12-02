f2663ab8cff1499f845498e8df3edd16541d4b2d
Mohamed KHALLOUKI <khallouki@et.esiea.fr>
2021-12-02 16:09:34 +0100

    Composition des Loggers

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -4,6 +4,6 @@ import java.util.function.Predicate;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        return new ContextualLogger(name, new FileLogger("logs.txt"));
+        return new CompositeLogger(new ContextualLogger(name, new FileLogger("logs.txt")), new ContextualLogger(name, new ConsoleLogger()));
     }
 }
